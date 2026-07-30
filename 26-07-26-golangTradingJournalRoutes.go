08c53795625abcd71e82febf6c883c391b23341f
Ghazi <ghazi.khairy@gmail.com>
2026-07-30 16:59:01 +0700

    Added GET route and POST route

--- 26-07-26-golangTradingJournalRoutes.go
+++ 26-07-26-golangTradingJournalRoutes.go
@@ -75,10 +75,43 @@ func createTrade (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Trade Entered", t.Asset, t.Direction, t.TraderID)
 }
 
+func getTrade(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		fmt.Fprintln(w, "Only POST method is allowed here")
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		fmt.Fprintln(w, "Please provide a trade id, e.g. ?id=1")
+		return
+	}
+
+	fmt.Fprintln(w, "You successfully requested trade id:", id)
+}
+
+func createTraderProfile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		fmt.Fprintln(w, "Only POST method is allowed here")
+		return
+	}
+
+	var t TraderProfile
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		fmt.Fprintln(w, "Error reading data", err)
+		return
+	}
+
+	fmt.Fprintln(w, "You successfully created a Trader Profile", t.PNL, t.WinRate, t.TraderID)
+}
+
 func main () {
 	http.HandleFunc("/traders", createTrader)
 	http.HandleFunc("/traders/get", getTrader)
 	http.HandleFunc("/trades", createTrade)
+	http.HandleFunc("/trades/get", getTrade)
+	http.HandleFunc("/traderprofile", createTraderProfile)
 	fmt.Println("Server running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
